e2851d8ddf452a2eeb2680bcabc281282d52abc8
Natan Nascimento <natanascimentom@outlook.com>
2020-12-01 18:39:58 -0300

    Alterando arquivo principal do projeto terraform

--- F113310/src/main.tf
+++ F113310/src/main.tf
@@ -1,3 +1,4 @@
+#Realizando uma requisição da hashicorp
 terraform {
   required_providers {
     aws = {
@@ -6,6 +7,9 @@ terraform {
     }
   }
 }
+#Informando para o Terraform, qual o provedor que será alocada a infraestrutura
+# Após definir o provedor, será definido um perfil que este perfil é definido lá na "\.aws\credentials"
+# E definimos por ultimo a região onde será alocada os recursos
 provider "aws" {
   profile = "awseducate"
   region  = "us-east-1"
